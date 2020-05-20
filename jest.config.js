e560d6926a741f0f64f79a0caee45a11acdc0d5a
Keit Oliveira <keitoliveira@outlook.com>
2020-05-20 01:18:41 -0300

    build(*): add config for Jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  setupFilesAfterEnv: [
+    '<rootDir>/jest.setup.js',
+  ],
+  moduleNameMapper: {
+    '^.+\\.(css|scss)$': 'identity-obj-proxy',
+  },
+  snapshotSerializers: [
+    'enzyme-to-json/serializer',
+    'jss-snapshot-serializer',
+  ],
+  coverageDirectory: '<rootDir>/coverage',
+  collectCoverageFrom: ['src/**/*.js'],
+};
